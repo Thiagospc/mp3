bd6a3749e0650fd0c66fa56e7a8386fb57b8b4cc
Thiagospc <thiago.spc1029@gmail.com>
2024-03-27 10:32:11 -0300

    começando as músicas

--- pages/albums.php
+++ pages/albums.php
@@ -1,5 +1,9 @@
 <h1>Álbums</h1>
 
+<a href="?page=new_album" class="btn btn-success">Adicionar novo Álbum</a>
+
+<hr>
+
 <div class="row">
     <?php
         
@@ -12,7 +16,7 @@
             $imgAlbum = "{$album}/{$nameAlbum}.jpeg";
     ?>
     <div class="col-3">
-        <a href="">
+        <a href="?page=music&album=<?=$nameAlbum?>">
             <img src="<?php echo $imgAlbum ?>" class="img-album">
             <?php echo $nameAlbum ?>
         </a>
---
+++ pages/new_album.php
@@ -0,0 +1,17 @@
+<a href="?page=albums">Voltar para os Álbums</a>
+<h1>cadastrar novo Álbum</h1>
+
+<form action="#" method="post">
+    <div class="form-group">
+        <input type="text" name="name" placeholder="Nome:" class="form-control">
+    </div>
+    <br />
+    <div class="form-group">
+        <input type="file" name="image" class="form-control">
+    </div>
+    <br />
+    <br />
+
+    <button type="submit" class="btn btn-success">Cadastrar</button>
+</form>
+
