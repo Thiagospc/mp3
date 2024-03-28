b6d6057a2accbf60d33da48d479533c22a688c4b
Thiagospc <thiago.spc1029@gmail.com>
2024-03-28 09:15:04 -0300

    músicas

--- helpers.php
+++ helpers.php
@@ -4,4 +4,10 @@
 
         return $albums;
     }
+
+    function getMusics($albums){
+        $musics = glob("albums/{$albums}/music/*.mp3");
+
+        return $musics;
+    }
 ?>
--- pages/music.php
+++ pages/music.php
@@ -1,2 +1,18 @@
+<a href="?page=albums">Voltar para os Álbums</a>
 <h1>Músicas do Álbum <?=$_GET['album']?></h1>
 <hr>
+
+<?php
+    $album = $_GET['album'];
+    $music = getMusics($album);
+
+    foreach ($music as $mp3){
+?>
+
+<div class="col-12">
+    <audio src="<?=$mp3?>" controls></audio>
+</div>
+
+<?php
+    }
+?>
