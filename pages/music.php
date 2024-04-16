d9202688384edb4d83557bbd218753eaf58f71ea
Thiago Santos Pinheiro Costa <thiagosantos@maiatecnologia.com.br>
2024-04-16 09:58:53 -0300

    adicionando músicas

--- pages/music.php
+++ pages/music.php
@@ -1,6 +1,7 @@
-<a href="?page=albums">Voltar para os Álbums</a>
 <h1>Músicas do Álbum <?=$_GET['album']?></h1>
-<hr>
+<p><a href="?page=albums">Voltar para os Álbums</a></p>
+
+<p><a href="?page=new_music&album=<?=$_GET['album']?>" class="btn btn-success">Adicionar novas músicas</a></p>
 
 <?php
     $album = $_GET['album'];
---
+++ pages/new_music.php
@@ -0,0 +1,33 @@
+<a href="?page=albums">Voltar para os Álbums</a>
+<h1>cadastrar novas músicas para <?=$_GET['album']?></h1>
+
+<form action="#" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+        <input type="file" name="audio" class="form-control">
+    </div>
+    <br />
+    <br />
+
+    <button type="submit" class="btn btn-success">Cadastrar</button>
+</form>
+
+<?php
+ini_set('upload_max_filesize', '1000M');
+ini_set('post_max_size', '1000M');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $album = $_GET['album'];
+
+    $path = "albums/{$album}/music";
+
+    if(!is_dir($path)){
+        mkdir($path);
+    }
+
+    if(move_uploaded_file($_FILES['audio']['tmp_name'], $path . '/' . $_FILES['audio']['name'])){
+        header("Location: ?page=music&album={$album}");
+    }else{
+        echo 'Falha no upload';
+    }
+}
+?>
